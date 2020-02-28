227582b3ffcd620304c2ee2451896783270812a6
Alexandre <external.z21amard@btwin.com>
2020-02-28 14:28:59 +0100

    import project
    chore: update git ignore
    
    chore: clean bitrise.yml
    
    fix; remove secrets

---
+++ buildspec.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/sliceutil"
+	"github.com/bitrise-io/go-utils/ziputil"
+	"github.com/bitrise-tools/go-steputils/output"
+	"github.com/bitrise-tools/go-steputils/tools"
+	"github.com/kballard/go-shellquote"
+	"github.com/ryanuber/go-glob"
+)
+
+type buildSpecification struct {
+	displayName          string
+	platformCmdFlag      string
+	platformSelectors    []string
+	outputPathPatterns   []string
+	additionalParameters string
+	projectLocation      string
+}
+
+func (spec buildSpecification) exportArtifacts(artifacts []string) error {
+	deployDir := os.Getenv("BITRISE_DEPLOY_DIR")
+	switch spec.platformCmdFlag {
+	case "aar":
+		return spec.exportAndroidArtifacts(artifacts, deployDir)
+	case "ios-framework":
+		return spec.exportIOSFramework(artifacts, deployDir)
+	default:
+		return fmt.Errorf("unsupported platform for exporting artifacts: %s. Supported platforms: apk, appbundle, ios", spec.platformCmdFlag)
+	}
+}
+
+func (spec buildSpecification) artifactPaths(outputPathPatterns []string, isDir bool) ([]string, error) {
+	var paths []string
+	for _, outputPathPattern := range outputPathPatterns {
+		pths, err := findPaths(spec.projectLocation, outputPathPattern, isDir)
+		if err != nil {
+			return nil, err
+		}
+		paths = append(paths, pths...)
+	}
+	return paths, nil
+}
+
+func (spec buildSpecification) exportIOSFramework(artifacts []string, deployDir string) error {
+	artifact := artifacts[len(artifacts)-1]
+	fileName := filepath.Base(artifact)
+
+	if len(artifacts) > 1 {
+		log.Warnf("- Multiple artifacts found: %v, exporting %s", artifacts, artifact)
+	}
+
+	if err := ziputil.ZipDir(artifact, filepath.Join(deployDir, fileName+".zip"), false); err != nil {
+		return err
+	}
+	log.Donef("- $BITRISE_DEPLOY_DIR/" + fileName + ".zip")
+
+	if err := tools.ExportEnvironmentWithEnvman("BITRISE_APP_DIR_PATH", artifact); err != nil {
+		return err
+	}
+	log.Donef("- $BITRISE_APP_DIR_PATH: " + artifact)
+
+	return nil
+}
+
+func (spec buildSpecification) exportAndroidArtifacts(artifacts []string, deployDir string) error {
+
+	var singleFileOutputEnvName string
+	var multipleFileOutputEnvName string
+	
+	singleFileOutputEnvName = "BITRISE_AAR_PATH"
+	multipleFileOutputEnvName = "BITRISE_AAR_PATH_LIST"
+
+	var deployedFiles []string
+	for _, path := range artifacts {
+		deployedFilePath := filepath.Join(deployDir, filepath.Base(path))
+
+		if err := output.ExportOutputFile(path, deployedFilePath, singleFileOutputEnvName); err != nil {
+			return err
+		}
+		deployedFiles = append(deployedFiles, deployedFilePath)
+	}
+	if err := tools.ExportEnvironmentWithEnvman(multipleFileOutputEnvName, strings.Join(deployedFiles, "\n")); err != nil {
+		return fmt.Errorf("failed to export enviroment variable %s, error: %s", multipleFileOutputEnvName, err)
+	}
+
+	log.Donef("- " + singleFileOutputEnvName + ": " + deployedFiles[len(deployedFiles)-1])
+	log.Donef("- " + multipleFileOutputEnvName + ": " + strings.Join(deployedFiles, "|"))
+	return nil
+}
+
+func (spec buildSpecification) buildable(platform string) bool {
+	return sliceutil.IsStringInSlice(platform, spec.platformSelectors)
+}
+
+func findPaths(location string, outputPathPattern string, dir bool) (out []string, err error) {
+	err = filepath.Walk(location, func(path string, info os.FileInfo, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
+
+		if !info.IsDir() == dir || !glob.Glob(outputPathPattern, path) {
+			return nil
+		}
+
+		out = append(out, path)
+		return nil
+	})
+	if len(out) == 0 && err == nil {
+		log.Debugf("couldn't find output artifact on path: " + filepath.Join(location, outputPathPattern))
+	}
+	return
+}
+
+func (spec buildSpecification) build(params string) error {
+	paramSlice, err := shellquote.Split(params)
+	if err != nil {
+		return err
+	}
+
+	var errorWriter io.Writer = os.Stderr
+	var errBuffer bytes.Buffer
+
+	buildCmd := command.New("flutter", append([]string{"build", spec.platformCmdFlag}, paramSlice...)...).SetStdout(os.Stdout)
+
+	if spec.platformCmdFlag == "ios-framework" {
+		buildCmd.SetStdin(strings.NewReader("a")) // if the CLI asks to input the selected identity we force it to be aborted
+		errorWriter = io.MultiWriter(os.Stderr, &errBuffer)
+	}
+
+	buildCmd.SetStderr(errorWriter)
+
+	fmt.Println()
+	log.Donef("$ %s", buildCmd.PrintableCommandArgs())
+	fmt.Println()
+
+	buildCmd.SetDir(spec.projectLocation)
+
+	err = buildCmd.Run()
+
+	return err
+}
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bitrise-io/go-steputils/stepconf"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+)
+
+// const ...
+const (
+)
+
+var flutterConfigPath = filepath.Join(os.Getenv("HOME"), ".flutter_settings")
+
+type config struct {
+	Platform                string              `env:"platform,opt[both,ios,android]"`
+	IOSExportPattern        string              `env:"ios_output_pattern,required"`
+	AndroidExportPattern    string              `env:"android_output_pattern,required"`
+	ProjectLocation         string              `env:"project_location,dir"`
+	DebugMode               bool                `env:"is_debug_mode,opt[true,false]"`
+}
+
+func failf(msg string, args ...interface{}) {
+	log.Errorf(msg, args...)
+	os.Exit(1)
+}
+
+func main() {
+	var cfg config
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Issue with input: %s", err)
+	}
+	stepconf.Print(cfg)
+	log.SetEnableDebugLog(cfg.DebugMode)
+
+	projectLocationAbs, err := filepath.Abs(cfg.ProjectLocation)
+	if err != nil {
+		failf("Failed to get absolute project path, error: %s", err)
+	}
+
+	exist, err := pathutil.IsDirExists(projectLocationAbs)
+	if err != nil {
+		failf("Failed to check if project path exists, error: %s", err)
+	} else if !exist {
+		failf("Project path does not exist.")
+	}
+
+	for _, spec := range []buildSpecification{
+		{
+			displayName:          "iOS",
+			platformCmdFlag:      "ios-framework",
+			platformSelectors:    []string{"both", "ios"},
+			outputPathPatterns:   append(strings.Split(cfg.IOSExportPattern, "\n")),
+		},
+		{
+			displayName:          "Android",
+			platformCmdFlag:      "aar",
+			platformSelectors:    []string{"both", "android"},
+			outputPathPatterns:   append(strings.Split(cfg.AndroidExportPattern, "\n")),
+		},
+	} {
+		if !spec.buildable(cfg.Platform) {
+			continue
+		}
+
+		spec.projectLocation = projectLocationAbs
+
+		fmt.Println()
+		log.Infof("Build " + spec.displayName)
+		if err := spec.build(spec.additionalParameters); err != nil {
+			failf("Failed to build %s platform, error: %s", spec.displayName, err)
+		}
+
+		fmt.Println()
+		log.Infof("Export " + spec.displayName + " artifact")
+
+		var artifacts []string
+		var err error
+
+		if spec.platformCmdFlag == "aar" {
+			artifacts, err = spec.artifactPaths(spec.outputPathPatterns, false)
+		} else {
+			artifacts, err = spec.artifactPaths(spec.outputPathPatterns, true)
+		}
+		if err != nil {
+			failf("failed to find artifacts, error: %s", err)
+		}
+
+		if err := spec.exportArtifacts(artifacts); err != nil {
+			failf("Failed to export %s artifacts, error: %s", spec.displayName, err)
+		}
+	}
+
+	fmt.Println()
+	log.Infof("Collecting cache")
+
+	if err := cacheCocoapodsDeps(projectLocationAbs); err != nil {
+		log.Warnf("Failed to collect cocoapods cache, error: %s", err)
+	}
+
+	if err := cacheCarthageDeps(projectLocationAbs); err != nil {
+		log.Warnf("Failed to collect carthage cache, error: %s", err)
+	}
+
+	if err := cacheAndroidDeps(projectLocationAbs); err != nil {
+		log.Warnf("Failed to collect android cache, error: %s", err)
+	}
+
+	if err := cacheFlutterDeps(projectLocationAbs); err != nil {
+		log.Warnf("Failed to collect flutter cache, error: %s", err)
+	}
+}
